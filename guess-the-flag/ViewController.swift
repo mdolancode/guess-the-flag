d4c2be486859fc09270693e482e5687b30c77fe3
Matthew Dolan <mdolancode@gmail.com>
2021-11-29 14:11:28 -0800

    Set title with correct answer.

--- guess-the-flag/ViewController.swift
+++ guess-the-flag/ViewController.swift
@@ -38,10 +38,13 @@ class ViewController: UIViewController {
     
     func askQuestion() {
         countries.shuffle()
+        
         button1.setImage(UIImage(named: countries[0]), for: .normal)
         button2.setImage(UIImage(named: countries[1]), for: .normal)
         button3.setImage(UIImage(named: countries[2]), for: .normal)
+        
         correctAnswer = Int.random(in: 0...2)
+        title = countries[correctAnswer].uppercased()
         
     }
 }
